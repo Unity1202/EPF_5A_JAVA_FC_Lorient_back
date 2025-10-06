7754246c10578da919d3463334ebb6f041675968
alexis.dhermy <a.dhermy@live.com>
2025-10-06 16:28:49 +0200

    add(initdb): création des tables

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,30 +1,96 @@
-create table students
+create table users
 (
     id SERIAL PRIMARY KEY,
-    first_name TEXT not null,
-    last_name TEXT not null,
-    birthdate date null,
-    major_id int null,
-    image bytea null
+    firstName TEXT not null,
+    lastName TEXT not null,
+    email TEXT not null,
+    password TEXT not null
 );
 
-create table majors
+create table admins
+(
+    id SERIAL PRIMARY KEY,
+    firstName TEXT not null,
+    lastName TEXT not null,
+    email TEXT not null,
+    password TEXT not null
+);
+
+create table articles
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT not null,
+    content TEXT not null,
+    image TEXT not null,
+    source TEXT not null,
+    createdAt TIMESTAMP not null
+);
+
+create table countries
 (
     id SERIAL PRIMARY KEY,
     name TEXT not null,
-    description TEXT not null
+    flag TEXT not null
 );
 
-create table courses
+create table coachs
+(
+    id SERIAL PRIMARY KEY,
+    firstName TEXT not null,
+    lastName TEXT not null,
+    dateOfBirth TIMESTAMP not null,
+    nationality INT not null,
+    FOREIGN KEY (nationality) REFERENCES countries(id),
+    crest TEXT not null
+);
+
+create table players
+(
+    id SERIAL PRIMARY KEY,
+    firstName TEXT not null,
+    lastName TEXT not null,
+    position TEXT not null,
+    dateOfBirth TIMESTAMP not null,
+    shirtNumber INT not null,
+    nationality INT not null,
+    FOREIGN KEY (nationality) REFERENCES countries(id),
+    crest TEXT not null
+);
+
+create table teams
 (
     id SERIAL PRIMARY KEY,
     name TEXT not null,
-    hours int not null
+    shortName TEXT not null,
+    crest TEXT not null,
+    rank INT not null,
+    coachId INT not null,
+    FOREIGN KEY (coachId) REFERENCES coachs(id)
 );
 
-create table student_course
+create table team_players
+(
+    teamId INT not null,
+    playerId INT not null,
+    PRIMARY KEY (teamId, playerId),
+    FOREIGN KEY (teamId) REFERENCES teams(id),
+    FOREIGN KEY (playerId) REFERENCES players(id)
+);
+
+create table matchs
 (
     id SERIAL PRIMARY KEY,
-    student_id int not null,
-    course_id int not null
+    utcDate TIMESTAMP not null,
+    status TEXT not null,
+    venue TEXT not null,
+    matchday INT not null,
+    lastUpdated TIMESTAMP not null,
+    winner TEXT not null,
+    winnerCode TEXT not null,
+    score_home INT not null,
+    score_away INT not null,
+    homeTeamId INT not null,
+    awayTeamId INT not null,
+    FOREIGN KEY (homeTeamId) REFERENCES teams(id),
+    FOREIGN KEY (awayTeamId) REFERENCES teams(id)
 );
