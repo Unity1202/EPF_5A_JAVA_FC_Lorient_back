79f5c60031e36ae0671d300cae9fc417e32ec016
ccristea <cornel.cristea@epfedu.fr>
2025-10-07 16:20:56 +0200

    Affichage joueurs fonctionnel

--- src/main/java/com/takima/backskeleton/controllers/PlayerController.java
+++ src/main/java/com/takima/backskeleton/controllers/PlayerController.java
@@ -1,14 +1,24 @@
 package com.takima.backskeleton.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.takima.backskeleton.DTO.PlayerDTO;
 import com.takima.backskeleton.mappers.PlayerMapper;
 import com.takima.backskeleton.models.Player;
 import com.takima.backskeleton.services.PlayerService;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
