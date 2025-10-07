1e3d02923cb2860147a0f9a03a7db652a6b1c6eb
Greg <gregoire.brun@epfedu.fr>
2025-10-07 16:33:32 +0200

    ajout de admin

---
+++ src/main/java/com/takima/backskeleton/models/Admin.java
@@ -0,0 +1,42 @@
+package com.takima.backskeleton.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="firstname")
+    private String firstName;
+    @Column(name="lastname")
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Admin() {}
+
+    public Admin(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
