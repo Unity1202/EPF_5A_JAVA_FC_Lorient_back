d18d15d8d0d3af4c6c921ec7d7a333ca0def7b0f
quentindhr <124066086+quentindhr@users.noreply.github.com>
2025-10-13 11:57:33 +0200

    CORS

--- src/main/java/com/takima/backskeleton/controllers/UserController.java
+++ src/main/java/com/takima/backskeleton/controllers/UserController.java
@@ -1,16 +1,26 @@
 package com.takima.backskeleton.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.takima.backskeleton.DTO.UserDTO;
 import com.takima.backskeleton.mappers.UserMapper;
 import com.takima.backskeleton.models.User;
 import com.takima.backskeleton.services.UserService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
