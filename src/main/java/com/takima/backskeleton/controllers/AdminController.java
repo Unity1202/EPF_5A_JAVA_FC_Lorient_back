a3c60b26b4faf94ed9418e38cc3e0b3864db47a0
quentindhr <124066086+quentindhr@users.noreply.github.com>
2025-10-13 11:59:03 +0200

    CORS2

--- src/main/java/com/takima/backskeleton/controllers/AdminController.java
+++ src/main/java/com/takima/backskeleton/controllers/AdminController.java
@@ -1,16 +1,26 @@
 package com.takima.backskeleton.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.takima.backskeleton.DTO.AdminDTO;
 import com.takima.backskeleton.mappers.AdminMapper;
 import com.takima.backskeleton.models.Admin;
 import com.takima.backskeleton.services.AdminService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admins")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
 
     private final AdminService adminService;
--- src/main/java/com/takima/backskeleton/controllers/ArticleController.java
+++ src/main/java/com/takima/backskeleton/controllers/ArticleController.java
@@ -1,16 +1,26 @@
 package com.takima.backskeleton.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.takima.backskeleton.DTO.ArticleDTO;
 import com.takima.backskeleton.mappers.ArticleMapper;
 import com.takima.backskeleton.models.Article;
 import com.takima.backskeleton.services.ArticleService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/articles")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ArticleController {
 
     private final ArticleService articleService;
